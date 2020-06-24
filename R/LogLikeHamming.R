951ca76cbdbae6cb6bc2fa42828c7fa8bec1d655
Ryan Hornby <ryanhornby1999@gmail.com>
2020-06-23 18:13:25 -0700

    Added weight calculation and plot arguments

--- R/LogLikeHamming.R
+++ R/LogLikeHamming.R
@@ -5,18 +5,20 @@
 #' @import superheat
 #' @export
 
-log_like_hamming       <- function(res, thresh = 0.975, risks_method = "linear")
+log_like_hamming = function(res, thresh = 0.975, risks_method = "linear",
+                            riskPlot = FALSE, scale_factor = 1.0,
+                            shift_factor = 0.0, weightsPlot = FALSE)
 {
   require(Rfast) ## needed for rowMaxs() and colMaxs()
   require(superheat) ## needed for heatmap
   log_lik           <- res$log_lik ## S x N matrix, including weighting
   N                 <- ncol(log_lik)
   S                 <- nrow(log_lik)
-  
+
   log_ratio         <- matrix(0,S,N)
   log_ratio_theta   <- matrix(0,S,1)
   pos               <- rep(TRUE,N)
-  
+
   for( s in 1:S )
   {
     log_like_xs  <- sum(log_lik[s,]) ## full data
@@ -31,7 +33,7 @@ log_like_hamming       <- function(res, thresh = 0.975, risks_method = "linear")
   log_ratio_theta         <- rowMaxs(log_ratio, value = TRUE) ## S x 1 (by row) maxima of log-ratio over records for *each* MCMC draw
   L                       <- quantile(log_ratio_theta,thresh)
   S_excl                  <- which(log_ratio_theta > L) ## Excluded MCMC iterations based on thres log_ratio_theta
-  
+
   ## remove rows from log_ratio for excluded MCMC observations so that weights tie to L
   if( !is.null(S_excl) && length(S_excl) > 0 )
   {
@@ -39,7 +41,7 @@ log_like_hamming       <- function(res, thresh = 0.975, risks_method = "linear")
   }else{
     logthresh_ratio         <- log_ratio
   }
-  
+
   ## Compute by record risks \in (0,1) - only include MCMC draws used to compose L
   log_ratio_data       <- colMaxs(logthresh_ratio,value=TRUE) ## 1 x N (by col) maxima of log-ratio over records for *each* record
   if( risks_method == "linear")
@@ -56,8 +58,34 @@ log_like_hamming       <- function(res, thresh = 0.975, risks_method = "linear")
       risks      <- f_logistic( log_ratio_data ) ## \in (0,1)
     }
   } ## end condition on which weight method to apply to convert L_i from R^+ to (0,1)
-  
-  return(list(log_ratio = log_ratio, logthresh_ratio = logthresh_ratio, log_ratio_theta = log_ratio_theta, 
-              log_ratio_data = log_ratio_data, S_excl = S_excl, L = L, risks = risks))
-  
+
+  if (riskPlot) {
+    dat_lr  <-  risks %>% as_tibble()
+    names(dat_lr) <- c("Risks")
+    ## density plot
+    print(
+      ggplot(dat_lr, aes(Risks)) +
+        geom_density(alpha=0.1, adjust = 1.5) + theme_bw() +
+        xlab("Record level Risks in (0,1)") + ylab("Density")
+    )
+  }
+
+  weights = scale_factor * (1 - risks) + shift_factor
+  weights[weights <= 0] = 0
+  weights[weights >= 1] = 1
+
+  if (weightsPlot) {
+    dat_lr  <-  weights %>% as_tibble()
+    names(dat_lr) <- c("Weights")
+    ## density plot
+    print(
+      ggplot(dat_lr, aes(Weights)) +
+        geom_density(alpha=0.1, adjust = 1.5) + theme_bw() +
+        xlab("Record level Weights") + ylab("Density")
+    )
+  }
+
+  return(list(log_ratio = log_ratio, logthresh_ratio = logthresh_ratio, log_ratio_theta = log_ratio_theta,
+              log_ratio_data = log_ratio_data, S_excl = S_excl, L = L, risks = risks, weights = weights))
+
 } ## end function log_like_hamming() to compute Lipschitz bound under Hamming-1 distance
